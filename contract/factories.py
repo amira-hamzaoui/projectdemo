94052ccc4ad363cc603b80fbda27ab05ff338120
amirahamzaoui <amira.hamzaoui@icloud.com>
2019-06-06 16:05:15 +0100

    contract test unitaire

--- contract/factories.py
+++ contract/factories.py
@@ -1,16 +1,15 @@
 from factory import DjangoModelFactory, SubFactory
-from .models import ContractTemplate
+from .models import Contract
 from datetime import date
-from roadmap.factories import YearFactory
 
-class ContractFactory(DjangoModelFactory):
+
+class ContractFactory (DjangoModelFactory):
     class Meta:
-        model = ContractTemplate
-        django_get_or_create = ('start','end')
+        model = Contract
+        django_get_or_create = ('start', 'end')
 
-    count_saison = 2
-    start = date(2017,2,12)
-    end = date(2018,2,11)
+    count_season = 2
+    start = date(2017, 2, 12)
+    end = date(2018, 2, 11)
     status = "True"
-    year = SubFactory(YearFactory)
-
+    year = 2018
